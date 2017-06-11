69bd71f39a493c924672b828bf3dfced5ffa568c
Snakebot <snakebott@gmail.com>
2017-06-11 13:56:37 +0300

    add method doc.add
    1. db.js: добавлен метод проверки токена
    2. Добавлен метод для добавления документа в базу

--- rpc_plugin/doc.js
+++ rpc_plugin/doc.js
@@ -12,7 +12,7 @@ const methods = {
     list: (args, opt, callback)=>{
         let errcode = 1001;
         logger.info(`<${className}.list>: incoming request params: ${JSON.stringify(args)}`);
-        db.read(module.parent.exports.getDBConnection, r.table('docs').withFields(['doc_name', 'doc_id']), errcode, args, opt, callback);
+        db.read(module.parent.exports.getDBConnection, r.table('docs').withFields(['name', 'doc_id']), errcode, args, opt, callback);
     },
 
     get: function(args, opt, callback){
@@ -36,8 +36,74 @@ const methods = {
     },
 
     add: function(args, opt, callback){
-        let errcode = 1006
-        callback(null, {msg: 'not yet ready'});
+        let errcode = 1006;
+        let access = 30;
+        let {token:token, doc:doc} = args;
+        if(!token || !doc || !doc.name || !doc.text){
+            logger.warn(`<${className}.add>: bad request incoming ${JSON.stringify(args)}`);
+            callback(new Error(`invalid parameters: errcode ${errcode}`));
+        }
+        else{
+            db.checkToken(module.parent.exports.getDBConnection, token).then((token)=>{
+                logger.info(`<${className}.add>: get token result ok`);
+                if(token.length === 0){
+                    logger.warn(`<${className}.add>: user auth error`);
+                    callback(new Error(`auth error`));
+                }
+                else{
+                    logger.info(`<${className}.add>: check token success`);
+                    if(token[0].access_id <= access){
+                        logger.warn(`<${className}>.add: user permisson denied operation access[${access}], 
+                            user access[${token[0].access_id}]`);
+                        callback(new Error(`permission denied`));
+                    }
+                    else{
+                        logger.info(`<${className}.add>: auth success`);
+                        r.table('docs').filter({name: doc.name}).run(module.parent.exports.getDBConnection()).then((cursor)=>{
+
+                            cursor.toArray().then((existDoc)=>{
+                                if(existDoc.length > 0){
+                                    logger.warn(`<${className}.add>: document with name ${doc.name} exists`);
+                                    callback(new Error(`document with name ${doc.name} exists`));
+                                }
+                                else{
+                                    r.table('docs').max({index: 'doc_id'}).run(module.parent.exports.getDBConnection()).then((lastDoc)=>{
+                                        let newDoc = {
+                                            access_id: token[0].access_id,
+                                            doc_id: lastDoc.doc_id + 1,
+                                            uploader: token[0].user_id,
+                                            name: doc.name,
+                                            text: doc.text,
+                                            upload_date: new Date().toISOString()
+                                        }
+                                        r.table('docs').insert(newDoc).run(module.parent.exports.getDBConnection()).then((result)=>{
+                                            logger.info(`<${className}.add>: document ${doc.name} inserted with doc_id ${newDoc.doc_id}`);
+                                            callback(null, {msg: 'document was added', doc_id: newDoc.doc_id});
+                                        }).catch((err)=>{
+                                            logger.error(`<${className}.add>: database insert error ${err.message}`);
+                                            db.error(err, errcode, callback);
+                                        });
+                                    }).catch((err)=>{
+                                        logger.error(`<${className}.add>: database error ${err.message}`);
+                                        db.error(err, errcode, callback);
+                                    });
+                                }
+                            }).catch((err)=>{
+                                logger.error(`<${className}.add>: database cursor error ${err.message}`);
+                                db.error(err, errcode, callback);
+                            });
+                        }).catch((err)=>{
+                            logger.error(`<${className}.add>: database error ${err.message}`);
+                            sb.error(err, errcode, callback);
+                        });
+                    }
+                }
+            }).catch((err)=>{
+                logger.error(`<${className}.add>: error get token, ${err.message}`);
+                db.error(err, errcode, callback);
+            });   
+        }
+
     },
 
     delete: function(args, opt, callback){

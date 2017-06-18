1eaea93e5f7bd1c791bc64ac8f8ecb022eaa62d0
Snakebot <snakebott@gmail.com>
2017-06-18 21:17:56 +0300

    add settings methods
    1. Добавлен метод setting.setname
    2. Добавлен метод setting.setdescription

--- rpc_plugin/setting.js
+++ rpc_plugin/setting.js
@@ -29,7 +29,41 @@ const methods = {
     },
 
     setname: function(args, opt, callback){
-        callback(null, {msg: 'not yet ready'});
+        let errcode =1104;
+        let access = 50;
+        let dbconn = module.parent.exports.getDBConnection();
+        let {token:token, name:name} = args;
+        if(!token || !name){
+            logger.warn(`<${className}.setname>: warn, bad parameters`);
+            callback(new Error(`invalid parameters`));
+        }
+        else{
+            logger.info(`<${className}.setname>: incoming update name ${JSON.stringify(args)}`);
+            db.checkToken(module.parent.exports.getDBConnection, token).then((key)=>{
+                logger.info(`<${className}.setname>: get token success`);
+                if(key.length === 0){
+                    logger.warn(`<${className}.setname>: bad token, auth error`);
+                    callback(new Error('auth error'));
+                }
+                else if(key[0].access_id < access){
+                    logger.warn(`<${className}.setname>: warn, permission denied`);
+                    callback(new Error(`permission denied`));
+                }
+                else{
+                    logger.info(`<${className}.setname>: aut success, try set name`);
+                    r.table('settings').update({name: name}).run(dbconn).then((result)=>{
+                        logger.info(`<${className}.setname>: name update success ${JSON.stringify(result)}`);
+                        callback(null, {msg: 'name updated', meta: result});
+                    }).catch((err)=>{
+                        logger.error(`<${className}.setname>: database error ${err.message}`);
+                        db.error(err, errcode, callback);
+                    });
+                }
+            }).catch((err)=>{
+                logger.error(`<${className}.setname>: error ${err.message}`);
+                db.error(err, errcode, callback);
+            });
+        }
     },
 
     setheaders: function(args, opt, callback){
@@ -37,7 +71,38 @@ const methods = {
     },
 
     setdescription: function(args, opt, callback){
-        callback(null, {msg: 'not yet ready'});
+        let errcode = 1106;
+        let access = 50;
+        let dbconn = module.parent.exports.getDBConnection();
+        let {token:token, description:description} = args;
+        if(!token || !description){
+            logger.warn(`<${className}.setdecription>: warn, bad parameters`);
+            callback(new Error('invalid parameters'));
+        }
+        else{
+            db.checkToken(module.parent.exports.getDBConnection, token).then((key)=>{
+                if(key.length === 0){
+                    logger.warn(`<${className}.setdescription>: warn, auth error ${JSON.stringify(args)}`);
+                    callback(new Error('auth error'));
+                }
+                else if(key[0].access_id < access){
+                    logger.warn(`<${className}.setdescription>: warn, permission denied ${JSON.stringify(args)}`);
+                    callback(new Error('permission denied'));
+                }
+                else{
+                    r.table('settings').update({description: description}).run(dbconn).then((result)=>{
+                        logger.info(`<${className}.setdescription>: description updated`);
+                        callback(null, {msg: 'description updated', meta: result});
+                    }).catch((err)=>{
+                        logger.error(`<${className}.setdesription>: database error ${err.message}`);
+                        db.error(err, errcode, callback);
+                    });
+                }
+            }).catch((err)=>{
+                logger.error(`<${className}.setdescription>: get token error ${err.message}`);
+                db.error(err, errcode, callback);
+            });
+        }
     }
 
 }
